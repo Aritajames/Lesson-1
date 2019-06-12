7548c7a2f6a892e639278dfc03d0eb21db9ae222
Aritajames <arita.jemo20@gmail.com>
2019-06-12 11:52:13 +0300

    Message

---
+++ Demo1.php
@@ -0,0 +1,24 @@
+<?php
+
+echo "Hello world";
+echo "<h1>Hello world!</h1>";
+
+#php case sensitivity
+//php case sensitivity
+
+/*
+php case sensitivity
+php case sensitivity
+*/
+
+//Key words in php are case insensitive
+//e.g if,echo,while,print
+
+ECHO "<h1>Hello world!</h1>";
+
+//Variables are case sensitive
+//Name and name are not the same
+
+
+?>
+
